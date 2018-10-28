45282ac276b8a15f2127807bae86028ebfb0403e
mariusz79 <mpawlowic@gmail.com>
2018-10-28 19:44:00 +0000

    add .mouseenter and .mouseleave events

--- script.js
+++ script.js
@@ -7,13 +7,13 @@ $(document).ready(function() {
 		$(".stream3").removeClass('highlight_stream');
 	  	$(".stream1").addClass('highlight_stream');
 	});
-	$("#stream2_btn").click(function () {
+	$("#stream2_btn").mouseenter(function () {					//works like hover
 		$(".stream1").removeClass('highlight_stream');
 		$(".stream2").removeClass('highlight_stream');
 		$(".stream3").removeClass('highlight_stream');
 	  	$(".stream2").addClass('highlight_stream');
 	});
-	$("#stream3_btn").click(function () {
+	$("#stream3_btn").mouseleave(function () {				//triggers when mouse leaves element
 		$(".stream1").removeClass('highlight_stream');
 		$(".stream2").removeClass('highlight_stream');
 		$(".stream3").removeClass('highlight_stream');
