e6a0a2a569b414af7190a086ab13e016e5124cc3
mariusz79 <mpawlowic@gmail.com>
2018-10-28 19:37:50 +0000

    using .click() event

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 $(document).ready(function() {
  
 
- 	$("#stream1_btn").on("click", function() {
+ 	$("#stream1_btn").click(function() {
  		$(".stream1").removeClass('highlight_stream');
 		$(".stream2").removeClass('highlight_stream');
 		$(".stream3").removeClass('highlight_stream');
 	  	$(".stream1").addClass('highlight_stream');
 	});
-	$("#stream2_btn").on("click", function() {
+	$("#stream2_btn").click(function () {
 		$(".stream1").removeClass('highlight_stream');
 		$(".stream2").removeClass('highlight_stream');
 		$(".stream3").removeClass('highlight_stream');
 	  	$(".stream2").addClass('highlight_stream');
 	});
-	$("#stream3_btn").on("click", function() {
+	$("#stream3_btn").click(function () {
 		$(".stream1").removeClass('highlight_stream');
 		$(".stream2").removeClass('highlight_stream');
 		$(".stream3").removeClass('highlight_stream');
