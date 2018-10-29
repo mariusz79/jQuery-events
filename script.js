0ee38b104036ec033612ab18c939b2138dc3beb5
mariusz79 <mpawlowic@gmail.com>
2018-10-29 12:55:29 +0000

    using mouseenter, mouseleave, click events

--- script.js
+++ script.js
@@ -19,6 +19,64 @@ $(document).ready(function() {
 		$(".stream3").removeClass('highlight_stream');
 	  	$(".stream3").addClass('highlight_stream');
 	});
-
-
+	$("p").click(function(){
+		$("p").addClass('red');
+	});
+	$("h2").mouseenter(function(){
+		$("h2").addClass('light-blue');
+	});
+	$("#h20").mouseenter(function () {
+		$("#h20").addClass('increase-font-size');
+		$("#h21").removeClass('increase-font-size');
+		$("#h22").removeClass('increase-font-size');
+		$("#h23").removeClass('increase-font-size');
+		$("#h24").removeClass('increase-font-size');
+		$("#h25").removeClass('increase-font-size');
+	});
+	$("#h21").mouseenter(function () {
+		$("#h21").addClass('increase-font-size');
+		$("#h20").removeClass('increase-font-size');
+		$("#h22").removeClass('increase-font-size');
+		$("#h23").removeClass('increase-font-size');
+		$("#h24").removeClass('increase-font-size');
+		$("#h25").removeClass('increase-font-size');
+	});
+	$("#h22").mouseenter(function () {
+		$("#h22").addClass('increase-font-size');
+		$("#h20").removeClass('increase-font-size');
+		$("#h21").removeClass('increase-font-size');
+		$("#h23").removeClass('increase-font-size');
+		$("#h24").removeClass('increase-font-size');
+		$("#h25").removeClass('increase-font-size');
+	});
+	$("#h23").mouseenter(function () {
+		$("#h23").addClass('increase-font-size');
+		$("#h21").removeClass('increase-font-size');
+		$("#h22").removeClass('increase-font-size');
+		$("#h20").removeClass('increase-font-size');
+		$("#h24").removeClass('increase-font-size');
+		$("#h25").removeClass('increase-font-size');
+	});
+	$("#h24").mouseenter(function () {
+		$("#h24").addClass('increase-font-size');
+		$("#h21").removeClass('increase-font-size');
+		$("#h22").removeClass('increase-font-size');
+		$("#h23").removeClass('increase-font-size');
+		$("#h20").removeClass('increase-font-size');
+		$("#h25").removeClass('increase-font-size');
+	});
+	$("#h25").mouseenter(function () {
+		$("#h25").addClass('increase-font-size');
+		$("#h21").removeClass('increase-font-size');
+		$("#h22").removeClass('increase-font-size');
+		$("#h23").removeClass('increase-font-size');
+		$("#h24").removeClass('increase-font-size');
+		$("#h20").removeClass('increase-font-size');
+	});
+	$(".bottom_button").mouseenter(function(){
+		$("body").css("background-color", "black");
+	});
+	$(".bottom_button").mouseleave(function () {
+		$("body").css("background-color", "grey");
+	});
 }); 
